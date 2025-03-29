ae6cf28fec3dcb492076ce11d370ae684fd9cdd8
ramadhan.yoga <ramadhan.yoga@students.utdi.ac.id>
2025-03-29 08:00:14 +0000

    Add: Menambahkan file konfigurasi Project IDX dan VSCode

--- pertemuan-4/praktik_pertemuan_4/lib/main.dart
+++ pertemuan-4/praktik_pertemuan_4/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key})
 
   // This widget is the root of your application.
   @override
